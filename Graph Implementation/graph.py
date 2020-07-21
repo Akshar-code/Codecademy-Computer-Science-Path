80b270665c89502064c7f9be63f975a714dc6b35
Akshar Kottuvada <akshar.kottuvada2000@gmail.com>
2020-07-21 13:45:50 +0530

    Add files via upload
    Graph Data Structure Implementation

---
+++ Graph Implementation/graph.py
@@ -0,0 +1,34 @@
+class Graph:
+  def __init__(self, directed = False):
+    self.graph_dict = {}
+    self.directed = directed
+
+  def add_vertex(self, vertex):
+    self.graph_dict[vertex.value] = vertex
+
+  def add_edge(self, from_vertex, to_vertex, weight = 0):
+    self.graph_dict[from_vertex.value].add_edge(to_vertex.value, weight)
+    if not self.directed:
+      self.graph_dict[to_vertex.value].add_edge(from_vertex.value, weight)
+
+  def find_path(self, start_vertex, end_vertex):
+    start = [start_vertex]
+    seen = {}
+    while len(start) > 0:
+      current_vertex = start.pop(0)
+      seen[current_vertex] = True
+      print("Visiting " + current_vertex)
+      if current_vertex == end_vertex:
+        return True
+      else:
+        vertex = self.graph_dict[current_vertex]
+        next_vertices = vertex.get_edges()
+        
+        # Filter next_vertices so it only
+        # includes vertices NOT IN seen
+        
+        # Checkpoint 3, uncomment and replace the question marks:
+        next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
+        start.extend(next_vertices)
+        
+    return False
---
+++ Graph Implementation/script.py
@@ -0,0 +1,29 @@
+from graph import Graph
+from vertex import Vertex
+
+railway = Graph()
+
+callan = Vertex('callan')
+peel = Vertex('peel')
+ulfstead = Vertex('ulfstead')
+harwick = Vertex('harwick')
+
+railway.add_vertex(callan)
+railway.add_vertex(peel)
+railway.add_vertex(harwick)
+railway.add_vertex(ulfstead)
+
+railway.add_edge(peel, harwick)
+railway.add_edge(harwick, callan)
+railway.add_edge(callan, peel)
+
+# Uncomment the code below when you're done refactoring!
+
+peel_to_ulfstead_path_exists = railway.find_path('peel', 'ulfstead')
+harwick_to_peel_path_exists = railway.find_path('harwick', 'peel')
+
+print("A path exists between peel and ulfstead:")
+print(peel_to_ulfstead_path_exists)
+print("A path exists between harwick and peel:")
+print(harwick_to_peel_path_exists)
+
